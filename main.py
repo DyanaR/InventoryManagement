4bbaddbeb52bc2835ad7d9effd2484d9f7cf2fd2
DyanaR <dyanarahhal98@gmail.com>
2025-05-04 04:50:09 -0500

    Add quick testing

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
  # Entry point: handles program flow
 
+from models.inventory import Inventory
+
+inv = Inventory()
+inv.add_product("Milk", 2.49, "Dairy", 20)
+inv.add_product("Eggs", 3.99, "Dairy", 12)
+
+for pid, product in inv.products.items():
+    print(f"ID: {pid}, Name: {product.name}, Price: {product.price}, Quantity: {product.quantity}")
+
+inv.total_inventory_value()
+
+inv.total_inventory_count()
+
+inv.adjust_product_price(2, 3.49)
+
+for pid, product in inv.products.items():
+    print(f"ID: {pid}, Name: {product.name}, Price: {product.price}, Quantity: {product.quantity}")
